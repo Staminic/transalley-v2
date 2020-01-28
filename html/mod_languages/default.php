dcf31669c6c7efaa933841da6c517420e9bcb73b
Simon Delmotte <simon.delmotte@staminic.com>
2020-01-28 18:51:59 +0100

    Corrections on the Megamenu CSS and langage switcher integration

--- html/mod_languages/default.php
+++ html/mod_languages/default.php
@@ -18,21 +18,28 @@ if ($params->get('dropdown', 0) && !$params->get('dropdownimage', 1))
 
 ?>
 <div class="mod-languages<?php echo $moduleclass_sfx; ?>">
-<?php if ($headerText) : ?>
-	<div class="pretext"><p><?php echo $headerText; ?></p></div>
-<?php endif; ?>
+
+<?php // if ($headerText) : ?>
+	<!-- <div class="pretext">
+		<p>
+			<?php // echo $headerText; ?>
+		</p>
+	</div> -->
+<?php // endif; ?>
 
 <?php if ($params->get('dropdown', 0) && !$params->get('dropdownimage', 1)) : ?>
 	<form name="lang" method="post" action="<?php echo htmlspecialchars_decode(htmlspecialchars(JUri::current(), ENT_COMPAT, 'UTF-8'), ENT_NOQUOTES); ?>">
-	<select class="inputbox advancedSelect" onchange="document.location.replace(this.value);" >
-	<?php foreach ($list as $language) : ?>
-		<option dir=<?php echo $language->rtl ? '"rtl"' : '"ltr"'; ?> value="<?php echo htmlspecialchars_decode(htmlspecialchars($language->link, ENT_QUOTES, 'UTF-8'), ENT_NOQUOTES); ?>" <?php echo $language->active ? 'selected="selected"' : ''; ?>>
-		<?php echo $language->title_native; ?></option>
-	<?php endforeach; ?>
-	</select>
+		<select class="inputbox advancedSelect" onchange="document.location.replace(this.value);" >
+		<?php foreach ($list as $language) : ?>
+			<option dir=<?php echo $language->rtl ? '"rtl"' : '"ltr"'; ?> value="<?php echo htmlspecialchars_decode(htmlspecialchars($language->link, ENT_QUOTES, 'UTF-8'), ENT_NOQUOTES); ?>" <?php echo $language->active ? 'selected="selected"' : ''; ?>>
+			<?php echo $language->title_native; ?></option>
+		<?php endforeach; ?>
+		</select>
 	</form>
+
 <?php elseif ($params->get('dropdown', 0) && $params->get('dropdownimage', 1)) : ?>
 	<div class="btn-group">
+
 		<?php foreach ($list as $language) : ?>
 			<?php if ($language->active) : ?>
 				<a href="#" data-toggle="dropdown" class="btn dropdown-toggle">
@@ -44,6 +51,7 @@ if ($params->get('dropdown', 0) && !$params->get('dropdownimage', 1))
 				</a>
 			<?php endif; ?>
 		<?php endforeach; ?>
+
 		<ul class="nav <?php echo $params->get('lineheight', 0) ? 'lang-block' : 'lang-inline'; ?> dropdown-menu" dir="<?php echo JFactory::getLanguage()->isRtl() ? 'rtl' : 'ltr'; ?>">
 		<?php foreach ($list as $language) : ?>
 			<?php if (!$language->active) : ?>
@@ -55,6 +63,7 @@ if ($params->get('dropdown', 0) && !$params->get('dropdownimage', 1))
 				<?php echo $language->title_native; ?>
 				</a>
 				</li>
+
 			<?php elseif ($params->get('show_active', 1)) : ?>
 				<?php $base = JUri::getInstance(); ?>
 				<li class="nav-item lang-active">
@@ -65,14 +74,17 @@ if ($params->get('dropdown', 0) && !$params->get('dropdownimage', 1))
 				<?php echo $language->title_native; ?>
 				</a>
 				</li>
+
 			<?php endif; ?>
 		<?php endforeach; ?>
 		</ul>
 	</div>
+
 <?php else : ?>
 	<ul class="nav <?php echo $params->get('inline', 1) ? 'lang-inline' : 'lang-block'; ?>" dir="<?php echo JFactory::getLanguage()->isRtl() ? 'rtl' : 'ltr'; ?>">
 	<?php foreach ($list as $language) : ?>
 		<?php if (!$language->active) : ?>
+
 			<li class="nav-item">
 			<a class="nav-link" href="<?php echo htmlspecialchars_decode(htmlspecialchars($language->link, ENT_QUOTES, 'UTF-8'), ENT_NOQUOTES); ?>">
 			<?php if ($params->get('image', 1)) : ?>
@@ -82,10 +94,11 @@ if ($params->get('dropdown', 0) && !$params->get('dropdownimage', 1))
 					<span class="label"><?php echo strtoupper($language->sef); ?></span>
 				<?php endif; ?>
 			<?php else : ?>
-				<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef); ?>
+				<?php echo $headerText; ?> <?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef); ?>
 			<?php endif; ?>
 			</a>
 			</li>
+
 		<?php elseif ($params->get('show_active', 1)) : ?>
 			<?php $base = JUri::getInstance(); ?>
 			<li class="nav-item lang-active">
